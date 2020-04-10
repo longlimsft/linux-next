84b4ab1c5e4a22830680d020110e772407368973
Long Li <longli@microsoft.com>
2020-04-09 23:49:09 -0700

    define vsp packet format

--- drivers/hv/hv_rimbaud.c
+++ drivers/hv/hv_rimbaud.c
@@ -9,18 +9,92 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/wait.h>
-#include <linux/sched.h>
-#include <linux/completion.h>
-#include <linux/string.h>
-#include <linux/mm.h>
-#include <linux/delay.h>
-#include <linux/init.h>
-#include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/hyperv.h>
-#include <linux/blkdev.h>
+
+enum rimbaud_request_type {
+	rimbaud_open_blob		= 1,
+	rimbaud_close_blob		= 2,
+	rimbaud_metadata_cache_hint	= 3,
+	rimbaud_get_glob		= 4,
+	rimbaud_update_session		= 5
+};
+
+// Header for all rimbaud request packets
+struct rimbaud_packet_request {
+	u32	length;
+	u32	blob_handle;
+
+	// The request type defined by enum rimbaud_request_type
+	u32	type;
+
+	// Request timeout in milliseconds
+	u32	timeout;
+	guid_t	transaction_id;
+
+	// Offset into the packet buffer for the sessin token
+	u32	session_token_offset;
+	u32	session_token_length;
+} __packed;
+
+struct rimbaud_open_blob_request {
+	struct rimbaud_packet_request header;
+
+	// Offset into the packet buffer for the blob name
+	u32	blob_name_offset;
+	u32	blob_name_length;
+} __packed;
+
+struct rimbaud_metadata_hint_request {
+	struct rimbaud_packet_request header;
+
+	// Offset into the blob where cached metadata should start
+	u64	offset;
+
+	// Length of the blob section to cache metadata
+	u64	length;
+} __packed;
+
+struct rimbaud_get_blob_request {
+	struct rimbaud_packet_request header;
+
+	// Request flags
+	union {
+		u32	all_flags;
+		struct {
+			u8	guest_compress : 1;
+			u8	guest_encrypt : 1;
+		};
+	};
+
+	// Offset into the blob to read
+	u64 offset;
+
+	// Length of the blob section to read
+	u32 length;
+} __packed;
+
+// Header for rimbaud response packets
+struct rimbaud_packet_response {
+	u32	length;
+	u32	status;
+} __packed;
+
+struct rimbaud_open_blob_response {
+	struct rimbaud_packet_response header;
+	u32 blob_handle;
+} __packed;
+
+struct rimbaud_get_blob_response {
+	struct rimbaud_packet_response header;
+
+	// Bytes written to data buffer
+	u32	bytes_written;
+
+	// Starting offset into the blob for returned data
+	u64	blob_offset;
+} __packed;
 
 static int rimbaud_ringbuffer_size = (128 * 1024);
 
